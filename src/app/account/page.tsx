694bceab68db05cf5ce2dc648a03c1e045f00dd3
rogmen1a2b3c <81282003+rogmen1a2b3c@users.noreply.github.com>
2023-10-23 00:45:18 -0700

    hola 2

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-
 import Breadcrumb from "../components/breadcrum";
-import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+import { BsBookmarkCheckFill } from "react-icons/bs";
+import { FaBookReader } from "react-icons/fa";
+import { GiReceiveMoney } from "react-icons/gi";
 
 const breadcrumbItems = [
   { href: "/", text: "Home" },
@@ -16,7 +17,7 @@ export default function AccountPage() {
     <>
       <div className="antialiased pt-10 relative mt-10 lg:mt-10">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-          <h1 className="text-5xl md:text-5xl font-bold mb-5 text-left text-gray-300 pb-4 md:pb-1">Challenges</h1>
+          <h1 className="text-5xl md:text-5xl font-bold mb-5 text-left text-gray-300 pb-4 md:pb-1">My Account</h1>
           <div className="pb-4 md:pb-4">
             <Breadcrumb items={breadcrumbItems} />
           </div>
@@ -24,7 +25,7 @@ export default function AccountPage() {
           <div className="flex flex-col sm:flex-row gap-4">
 
             <div className="w-full md:w-1/3 ">
-              <div id="left" className="h-full bg-gray-800 rounded-lg">
+              <div id="left" className="h-full glass-box rounded-lg">
                 Columna Izquierda
               </div>
             </div>
@@ -32,38 +33,43 @@ export default function AccountPage() {
 
             <div id="right" className="md:w-2/3 w-full">
 
-              <div className="md:flex  mb-4  gap-x-4 gap-y-4">
+              <div className="md:flex mb-4 gap-x-4 gap-y-4">
                 
-                <div id="col1" className="w-full rounded-lg flex mb-4 md:mb-0 items-center md:basis-1/4 p-4 bg-gray-800 ">
-                 <ArrowDownCircleIcon className="h-8 w-8 mr-6"/>
-                   <div className="text-right">
-                    <p className="mb-2 text-sm  font-medium text-gray-400">
-                      Total lessons
-                    </p>
-                    <p className="text-lg font-semibold  text-xl text-gray-200">
-                      20
-                    </p>
+                <div id="col1" className="basis-1/2 flex rounded-lg flex mb-4 md:mb-0 items-center md:basis-1/4 p-4 glass-box ">
+                 <FaBookReader className="h-10 w-10 mx-auto"/>
+                   <div className="text-right mx-auto">
+                    <p className="mb-2 text-sm  font-medium text-gray-400">Total lessons</p>
+                    <p className="text-lg font-semibold  text-xl text-gray-200">20</p>
                   </div>
                 </div>
 
-                <div id="col2" className="w-full rounded-lg md:basis-1/4 mb-4 md:mb-0 p-4 bg-gray-800">
-                  Columna B
+                <div id="col3" className="basis-1/2 dlex rounded-lg flex mb-4 md:mb-0 items-center md:basis-1/4 p-4 glass-box ">
+                 <GiReceiveMoney className="h-10 w-10 mx-auto"/>
+                   <div className="text-right mx-auto">
+                    <p className="mb-2 text-sm  font-medium text-gray-400">VKRs Owned</p>
+                    <p className="text-lg font-semibold  text-xl text-gray-200">2256</p>
+                  </div>
                 </div>
 
-                <div id="col3" className="w-full rounded-lg md:basis-1/2 mb-4 md:mb-0 p-4 bg-gray-800">
-                  Columna C
+                <div id="col3" className="w-full rounded-lg flex mb-4 md:mb-0 items-center md:basis-1/2 p-4 glass-box ">
+                 <BsBookmarkCheckFill className="h-10 w-10 mx-auto"/>
+                   <div className="text-right mx-auto">
+                    <p className="mb-2 text-sm  font-medium text-gray-400">Education topics with best performance</p>
+                    <p className="text-lg font-semibold  text-xl text-gray-200">Cryptocurrency / NFTs</p>
+                  </div>
                 </div>
+
               </div>
 
               <div className="mb-4">
-                <div className="w-full p-4 rounded-lg bg-gray-800">
+                <div className="w-full p-4 rounded-lg glass-box">
                   Columna Simple 1
                 </div>
               </div>
 
 
               <div>
-                <div className="w-full p-4 rounded-lg bg-gray-800">
+                <div className="w-full p-4 rounded-lg glass-box">
                   Columna Simple 2
                 </div>
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,42 +51,61 @@ export default function Home() {
     router.push("/account");
   };
 
+
+
   return (
     <>
-      <div className="antialiased  pt-10 md:pt-20 relative bg-lines mt-10 lg:mt-1 ">
+    <div className="antialiased  relative  md:pt-20 ">
+        <div className=" mx-auto max-w-screen-xl text-center ">
+     
+        <div className="antialiased  relative   ">
 
+        <div className="flex flex-col md:flex-row">
 
-
-        <div className="py-2 px-4 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-12">
-          {/* <a href="https://vrakka.com" target="_blank" className="inline-flex justify-between bg-black items-center py-1 px-1 pr-4 mb-7 text-sm  rounded-full  text-white  hover:bg-gray-900" role="alert">
-            <span className="text-xs bg-primary-500 rounded-full text-white px-4 py-1.5 mr-3">Are you ready?</span> <span className="text-sm font-medium">Visit vrakka.com</span>
-            <svg className="ml-2 w-10 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </a> */}
-          <h1 className="text-2xl md:text-4xl font-semibold mb-2 text-gray-300">Where Innovation Meets</h1>
-          <p className="text-5xl md:text-7xl font-semibold  mb-5  bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">Web 3.0 Education</p>
-          <div className="flex justify-center items-center pb-6 ">
-            <VideoWelcome
-              thumb={VideoThumb}
-              thumbWidth={500}
-              thumbHeight={432}
-              thumbAlt="Modal video thumbnail"
-              video="/videos/vrakka.mp4"
-              videoWidth={560}
-              videoHeight={315} />
-          </div>
-          <p className="mb-2 text-white text-base font-normal  lg:text-lg sm:px-16 xl:px-20 ">
-            In our academy, we are not only teaching about cryptocurrencies and blockchain but also contributing to building a more inclusive and equitable future through technology. We invite you to join this mission"</p>
-          {/* <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#" className="inline-flex text-gray-100 justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-primary-800 focus:ring-4 focus:ring-primary-900">
-              Start Now
-              <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-200 rounded-lg border border-gray-300  focus:ring-4  border-gray-700 hover:bg-gray-700 focus:ring-gray-800">
+          <div className="w-full md:w-1/2 py-4">
+            <div className="py-2 px-4 mx-auto max-w-screen-xl text-center md:text-left md:justify-left lg:pt-8 md:pl-2">
+              <a href="https://vrakka.com" target="_blank" className="inline-flex justify-between bg-black items-center py-1 px-1 pr-4 mb-7 text-sm  rounded-full  text-white  hover:bg-gray-900" role="alert">
+                <span className="text-xs bg-primary-500 rounded-full text-white px-4 py-1.5 mr-3">Are you ready?</span>
+                <span className="text-sm font-medium">Visit vrakka.com</span>
+                <svg className="ml-2 w-10 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+              </a>
+              <h1 className="text-2xl md:text-3xl font-semibold mb-4 text-gray-300">Where Innovation Meets</h1>
+              <p className="text-5xl md:text-6xl font-semibold  mb-8  bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient">Web 3.0 Education</p>
+              <div className="md:text-left">
+                <p className="mb-2 text-white text-base font-normal lg:text-lg   ">
+                  In our academy, we are not only teaching about cryptocurrencies and blockchain but also contributing to building a more inclusive and equitable future through technology. We invite you to join this mission"</p>
+              </div>
+              <div className="flex flex-col mt-10 mb-8 lg:mb-10 space-y-4 sm:flex-row md:justify-start sm:space-y-0 sm:space-x-4">
+                <a href="/register" className="inline-flex text-gray-100 justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg bg-primary-800 focus:ring-4 focus:ring-primary-900">
+                  Start Now
+                  <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+                {/* <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-200 rounded-lg border border-gray-300  focus:ring-4  border-gray-700 hover:bg-gray-700 focus:ring-gray-800">
               <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
               Watch video
-            </a>
-          </div> */}
+            </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/2  flex flex-col justify-center items-center">
+  <div className="text-center p-2">
+    <VideoWelcome
+      thumb={VideoThumb}
+      thumbWidth={900}
+      thumbHeight={732}
+      thumbAlt="Vrakka Academy"
+      video="/videos/vrakka.mp4"
+      videoWidth={660}
+      videoHeight={415}
+    />
+  </div>
+</div>
         </div>
+
+
+
+
       </div>
       <Spotlightscards />
       <HowWork />
@@ -98,7 +117,10 @@ export default function Home() {
         <Carrousel />
       </div>
       <Byvrakka />
-     
+
+      </div>
+      </div>
+
     </>
   );
 }
